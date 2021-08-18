85e4f42b05ffd90b87de97648d8ed3956d06e654
Guillermo Paredes <gdavid.ptorrez@gmail.com>
2021-08-18 11:10:35 -0400

    IMP: Add set get @Input

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -9,8 +9,17 @@ import { TTimeline } from './types';
 export class TimelineComponent implements OnInit {
   @Input() leftContainer?: TemplateRef<any>;
   @Input() rightContainer?: TemplateRef<any>;
-  @Input() data?: TTimeline[] = [];
+  @Input()
+  get data(): TTimeline[] {
+    return this._data;
+  }
+  set data(data: TTimeline[]) {
+    this._data = [...data];
+  }
+  _data: TTimeline[] = [];
+
   @Input() reverse?: boolean = false;
+
   constructor() {}
 
   ngOnInit(): void {
